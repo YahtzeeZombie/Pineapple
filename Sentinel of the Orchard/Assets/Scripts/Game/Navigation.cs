6d411e649db5c38a38cc297a9ca8e8f30dcd520f
shadowgoer <benjamin.dehler@gmail.com>
2015-09-04 14:14:07 -0500

    Changes

---
+++ Sentinel of the Orchard/Assets/Scripts/Game/Navigation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+		Transform nexus;               // Reference to the player's position.
+		
+		NavMeshAgent nav;               // Reference to the nav mesh agent.
+		
+		
+		void Awake ()
+		{
+			// Set up the references.
+			nexus = GameObject.FindGameObjectWithTag ("nexus").transform;
+			
+			nav = GetComponent <NavMeshAgent> ();
+		}
+		
+		
+		void Update (){
+			
+				// ... set the destination of the nav mesh agent to the player.
+				nav.SetDestination (nexus.position);
+			
+			
+
+	}
+}
